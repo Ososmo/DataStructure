8100330c727a5b9b2994f7aa35631be7aedc6c62
Osama Mohamed <115228796+Ososmo@users.noreply.github.com>
2024-05-13 21:55:26 +0300

    Update main.cpp

--- assginment2/main.cpp
+++ assginment2/main.cpp
@@ -1,5 +1,6 @@
 #include<bits/stdc++.h>
 #include "Item.h"
+#include "HeapSort.h"
 using namespace std;
 vector<Item>items;
 void get_input()
@@ -15,7 +16,7 @@ void get_input()
     int sz=numberOfStudents;
     while (sz--) {
         getline(inputFile, name);
-        inputFile >> price >> category;
+        inputFile >> category>>price;
         inputFile.ignore();
         items.push_back(Item(name, price,category ));
         numStudents++;
@@ -25,24 +26,65 @@ void get_input()
 }
 void dispaly()
 {
-  cout<<"1-Heap sort\n";
+  cout<<"1-Heap sort with name Asec\n";
+  cout<<"2-Heap sort with name Desc\n";
+  cout<<"3-Heap sort with price Asec\n";
+  cout<<"4-Heap sort with price Desc\n";
   cout<<"2-AVL\n";
   cout<<"3-Binary Search\n";
-  
+
 
 }
 int main() {
     get_input();
-    while(true)
+    int choice;
+    while(choice)
     {
         dispaly();
-        int choice;cin>>choice;
-        if(choice)
-        {
-            // do something
+        cin>>choice;
+
+        switch (choice) {
+            case 1:
+            {
+
+                SortByName *sort_by_name_Asec=new SortByName(2);
+                Build_Heap hp(items,sort_by_name_Asec);
+                hp.build();
+                hp.display();
+                break;
+            }
+            case 2:
+            {
+
+                SortByName *sort_by_name_Desc=new SortByName(1);
+                Build_Heap hp(items,sort_by_name_Desc);
+                hp.build();
+                hp.display();
+                break;
+            }
+            case 3:
+            {
+
+                SortByPrice *sort_by_price_Asec=new SortByPrice(2);
+                Build_Heap hp(items,sort_by_price_Asec);
+                hp.build();
+                hp.display();
+                break;
+            }
+            case 4:
+            {
+
+                SortByPrice *sort_by_price_Desc=new SortByPrice(1);
+                Build_Heap hp(items,sort_by_price_Desc);
+                hp.build();
+                hp.display();
+                break;
+            }
+            default:
+                break;
+
         }
-        else
-            break;
+        system("PAUSE");
     }
     return 0;
 }
